2ce69c371a95e5b641c049bfa56177381175ff8b
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-06-10 15:23:07 -0400

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,3 +1,42 @@
+-- Main.hs
+
+type Coordenada = (Int, Int)
+type Bosque = [[Int]]
+
+data Estado = Estado {
+  pos :: Coordenada,
+  energia :: Int,
+  camino :: [Coordenada]
+} deriving (Show)
+
+valorEn :: Bosque -> Coordenada -> Int
+valorEn bosque (i,j) = (bosque !! i) !! j
+
+valida :: Bosque -> Coordenada -> Bool
+valida bosque (i,j) = 
+  i >= 0 && j >= 0 && i < length bosque && j < length (head bosque)
+
+ajustarEnergia :: Bosque -> Coordenada -> Bool -> Int -> Int
+ajustarEnergia bosque c esDiagonal energiaActual =
+  let base = energiaActual + valorEn bosque c
+      penalizacionTrampa = if valorEn bosque c == 0 then 3 else 0
+      penalizacionDiagonal = if esDiagonal then 2 else 0
+  in base - penalizacionTrampa - penalizacionDiagonal
+
+movimientos :: Bosque -> Estado -> [Estado]
+movimientos bosque (Estado (i,j) e caminoAnt) = 
+  let candidatos = [ ((i, j+1), False), ((i+1, j), False), ((i+1, j+1), True),
+                     ((i, j-1), False), ((i-1, j), False) ]
+      filtrados = filter (\(c, _) -> valida bosque c && notElem c caminoAnt) candidatos
+      siguientes = [ Estado c (ajustarEnergia bosque c diag e) (caminoAnt ++ [c]) | (c, diag) <- filtrados, ajustarEnergia bosque c diag e >= 0 ]
+  in siguientes
+
+explorar :: Bosque -> Coordenada -> Coordenada -> [Estado] -> [Estado]
+explorar _ _ _ [] = []
+explorar bosque fin destino (x:xs)
+  | pos x == destino = x : explorar bosque fin destino xs
+  | otherwise = explorar bosque fin destino (movimientos bosque x ++ xs)
+
 main :: IO ()
 main = do
   putStrLn "Iniciando el bosque mágico..."

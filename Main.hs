2d9124e4776b7e9f821d7b1acf3c7f7968b19317
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-06-10 15:30:01 -0400

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
--- Main.hs
+import Debug.Trace
 
 type Coordenada = (Int, Int)
 type Bosque = [[Int]]
@@ -23,31 +23,31 @@ ajustarEnergia bosque c esDiagonal energiaActual =
       penalizacionDiagonal = if esDiagonal then 2 else 0
   in base - penalizacionTrampa - penalizacionDiagonal
 
+-- MOVIMIENTOS LIMITADOS: solo derecha, abajo, y diagonal abajo-derecha
 movimientos :: Bosque -> Estado -> [Estado]
 movimientos bosque (Estado (i,j) e caminoAnt) = 
-  let candidatos = [ ((i, j+1), False), ((i+1, j), False), ((i+1, j+1), True),
-                     ((i, j-1), False), ((i-1, j), False) ]
+  let candidatos = [ ((i, j+1), False), ((i+1, j), False), ((i+1, j+1), True) ]
       filtrados = filter (\(c, _) -> valida bosque c && notElem c caminoAnt) candidatos
-      siguientes = [ Estado c (ajustarEnergia bosque c diag e) (caminoAnt ++ [c]) | (c, diag) <- filtrados, ajustarEnergia bosque c diag e >= 0 ]
+      siguientes = [ Estado c (ajustarEnergia bosque c diag e) (caminoAnt ++ [c])
+                   | (c, diag) <- filtrados
+                   , ajustarEnergia bosque c diag e >= 0 ]
   in siguientes
 
 explorar :: Bosque -> Coordenada -> Coordenada -> [Estado] -> [Estado]
 explorar _ _ _ [] = []
 explorar bosque fin destino (x:xs)
-  | pos x == destino = x : explorar bosque fin destino xs
+  | pos x == destino = trace ("Llega a destino: " ++ show (camino x)) $
+                       x : explorar bosque fin destino xs
   | otherwise = explorar bosque fin destino (movimientos bosque x ++ xs)
 
 main :: IO ()
 main = do
-  putStrLn "Iniciando el bosque mágico..."
-
-  let bosque = [[2,-3,1,0,2,3],
-                [-5,4,-2,1,0,-4],
-                [1,3,0,-3,2,2],
-                [2,-1,4,0,-5,1],
-                [0,2,-3,3,4,-1],
-                [1,0,2,-2,1,5]]
-  let energiaInicial = 12
+  putStrLn "Iniciando el bosque mágico... (versión optimizada)"
+  
+  let bosque = [[2, -1, 3],
+                [0, 4, -2],
+                [1, -3, 5]]
+  let energiaInicial = 10
 
   let inicio = (0,0)
   let energiaInicialReal = ajustarEnergia bosque inicio False energiaInicial
